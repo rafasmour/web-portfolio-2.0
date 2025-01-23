7249447057dcbf76ece4cafaeea8bd573e802761
rafasmour <rafasmour@gmail.com>
2025-01-23 09:28:01 +0200

    making footer added font-awesome kit

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import SubTitle from '../Blocks/SubTitle'
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-    <div>
-        <div></div>
+    <footer className="w-full">
+    <div className="flex flex-row justify-around p-4">
+        <div className="flex flex-col justify-between">
+           <SubTitle context="Contact"/>
+           <ul className="list-none">
+              <li></li>
+              <li></li>
+              <li></li>
+           </ul>
+        </div>
         <div></div>
     </div>
       <h2 className='text-center text-light-primary dark:text-dark-primary text-sm md:text-base'>&copy; Rafail Mourouzidis 2025</h2>
